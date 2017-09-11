f9f7a0ac5bf70f5753ea80b73583bd08a789d75b
jblandis <31359204+jblandis@users.noreply.github.com>
2017-09-11 15:59:33 -0400

    Update Diversification_shifts_randomized.py

--- Diversification_shifts_randomized.py
+++ Diversification_shifts_randomized.py
@@ -2,20 +2,15 @@ import random
 
 #create log file to write summed values after each generation
 log_file = open("Four_node_lag_permutation.txt", "w")
-log_file
-
-#lst = []
-
-#for i in range(7860,15718):
-#	lst.append(i)
 
 #create a dictionary with all of the nodes that an upshift occurs
 keys = []
 values = []
-#open the file text file with nodes and shifts, and strip out new line information
+#open the file text file with nodes in one column and the second column having a 1 to demonstrate a shift; 
+#strip out new line information
 with open("Four_node_lag.txt", "r") as f:
 	words = [word.strip() for word in f]
-# for each line in the file, the second column is added to keys, and the third column to the values variable
+# for each line in the file, the first column is added to keys, and the second column to the values variable
 for line in words:
 	code = line.split("\t")
 	keys.append(code[0])
@@ -23,7 +18,7 @@ for line in words:
 #this creates the dictionary from keys and values varibles
 new_dict = {k: v for k, v in zip(keys, values)}
 
-#open text document that was made listing all possible nodes where a WGD could occur in order
+#open text document that was made listing all possible nodes where a WGD could occur in order, all nodes in one column
 nodes = open("Nodes.txt", "r")
 lines = []
 for line in nodes:
@@ -31,6 +26,7 @@ for line in nodes:
 	WGD = data[0]
 	lines.append(WGD)
 
+#perform 100,000 randomizations selecting 251 nodes to be randomized shifts in diversification. 	
 for x in range(1,100001):
 	rando = random.sample(lines,251)
 	#print(rando)
@@ -40,11 +36,9 @@ for x in range(1,100001):
 	#save all values from randomized in a variable
 	m = n.values()
 
+	#scroll through the variable, count all the randomized nodes and add up all that have a 1, representing a match
 	total = 0
 	for i in m:
 		total = total + int(i)
 	
 	log_file.write(str(total)+ "\n")	
-	 
-	#random.shuffle(WGD)
-#	print(str(WGD))
