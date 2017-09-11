1d808c9fd4cf8cf6fcd2b9c8a406452d55f34c3a
jblandis <31359204+jblandis@users.noreply.github.com>
2017-09-11 16:03:34 -0400

    Update GetSpeciesRichness.py

--- GetSpeciesRichness.py
+++ GetSpeciesRichness.py
@@ -3,10 +3,10 @@ import os
 #create a dictionary with all of the nodes that an upshift occurs
 keys = []
 values = []
-#open the file text file with nodes and shifts, and strip out new line information
+#open the file text file with species in one column and number of species in the second column, and strip out new line information
 with open("SpeciesRichness.txt", "r") as f:
 	words = [word.strip() for word in f]
-# for each line in the file, the second column is added to keys, and the third column to the values variable
+# for each line in the file, the first column is added to keys, and the second column to the values variable
 for line in words:
 	code = line.split("\t")
 	keys.append(code[0])
@@ -14,9 +14,11 @@ for line in words:
 #this creates the dictionary from keys and values varibles
 new_dict = {k: v for k, v in zip(keys, values)}
 
-
+#path of folder with all descendant files generated in R
 folder_path = '/Users/jacoblandis/Desktop/SisterCladeSim/Descendant_files'
 
+#open each file path sequentially, attempt to match descendant node to dictionary, if matching, add that number of species.  If no match,
+#report a 0 for that row
 for data_file in sorted(os.listdir(folder_path)):
 	filename = "%s" %data_file
 	filepath = '/Users/jacoblandis/Desktop/SisterCladeSim/Descendant_files/%s' % (data_file)
@@ -30,6 +32,7 @@ for data_file in sorted(os.listdir(folder_path)):
 		logfile.close
 
 
+#open count files for each node, sum them up and report back in a single log file		
 Count_log = open("Count_log.txt", "w")
 parsed_path = '/Users/jacoblandis/Desktop/SisterCladeSim/Parsed_data'
 for data_file2 in sorted(os.listdir(folder_path)):
